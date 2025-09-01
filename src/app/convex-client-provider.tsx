a4dfebf4e374aef8adeef98f2933d0cb6c80ba31
Ron Almog <ron8132@gmail.com>
2025-09-01 07:14:01 -0400

    add convex authentication

--- src/app/convex-client-provider.tsx
+++ src/app/convex-client-provider.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { ConvexReactClient, ConvexProvider } from "convex/react";
+import { ConvexReactClient } from "convex/react";
+import { ConvexAuthNextjsProvider } from "@convex-dev/auth/nextjs";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
@@ -9,5 +10,9 @@ export function ConvexClientProvider({
 }: {
   children: React.ReactNode;
 }) {
-  return <ConvexProvider client={convex}>{children}</ConvexProvider>;
+  return (
+    <ConvexAuthNextjsProvider client={convex}>
+      {children}
+    </ConvexAuthNextjsProvider>
+  );
 }
