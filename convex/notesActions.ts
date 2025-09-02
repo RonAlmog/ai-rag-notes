b1a4a7ef1e40987336fe759024aa4d5466da5e89
Ron Almog <ron8132@gmail.com>
2025-09-02 19:09:02 -0400

    findRelevantNotes

--- convex/notesActions.ts
+++ convex/notesActions.ts
@@ -4,11 +4,11 @@
 // because for creating the embedding we need to call generateEmbeddings,
 // that is using @ai-sdk/openai. this is not possible from convex.
 import { v } from "convex/values";
-import { action } from "./_generated/server";
+import { action, internalAction } from "./_generated/server";
 import { getAuthUserId } from "@convex-dev/auth/server";
-import { generateEmbeddings } from "../src/lib/embeddings";
+import { generateEmbedding, generateEmbeddings } from "../src/lib/embeddings";
 import { internal } from "./_generated/api";
-import { Id } from "./_generated/dataModel";
+import { Doc, Id } from "./_generated/dataModel";
 
 export const createNote = action({
   args: {
@@ -35,5 +35,40 @@ export const createNote = action({
       }
     );
     return noteId;
+  },
+});
+
+// internalAction can only be called from within other convex actions, not from frontend.
+export const findRelevantNotes = internalAction({
+  args: {
+    userId: v.id("users"),
+    query: v.string(),
+  },
+  handler: async (ctx, args): Promise<Array<Doc<"notes">>> => {
+    const { userId, query } = args;
+
+    const embedding = await generateEmbedding(query);
+
+    // now lets do a vector search
+    const results = await ctx.vectorSearch("noteEmbeddings", "by_embedding", {
+      vector: embedding,
+      limit: 16,
+      filter: (q) => q.eq("userId", userId),
+    });
+
+    console.log("Vector search results:", results);
+
+    // take only meaningful results
+    const resultsAboveThreshold = results.filter(
+      (result) => result._score > 0.3
+    );
+
+    const embeddingIds = resultsAboveThreshold.map((result) => result._id);
+
+    const notes = await ctx.runQuery(internal.notes.fetchNotesByEmbeddingIds, {
+      embeddingIds,
+    });
+
+    return notes;
   },
 });
