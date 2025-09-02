858d6a6602c636e6a0123f2ca42dada89ef8dc57
Ron Almog <ron8132@gmail.com>
2025-09-02 07:59:49 -0400

    solve cors issue

--- convex/http.ts
+++ convex/http.ts
@@ -22,13 +22,49 @@ http.route({
 
     const result = streamText({
       model: openai("gpt-3.5-turbo"), // gpt-4o-mini
-      system: "You are a helpful assistant.",
+      system: `
+      You are a helpful assistant that can search through the user's notes.
+      Use the information from the notes to answer questions and provide insights.
+      If the requested information is not available, respond with "Sorry, I can't find that information in your notes".
+      You can use markdown formatting like links, bullet points, numbered lists, and bold text.
+      Provide links to relevant notes using this relative URL structure (omit the base URL): '/notes?noteId=<note-id>'.
+      Keep your responses concise and to the point.
+      `,
       messages: convertToModelMessages(lastMessages),
       onError: (error) => {
         console.error("Error occurred while streaming text:", error);
       },
     });
-    return result.toUIMessageStreamResponse();
+    return result.toUIMessageStreamResponse({
+      headers: new Headers({
+        "Access-Control-Allow-Origin": "*",
+        Vary: "origin",
+      }),
+    });
+  }),
+});
+
+http.route({
+  path: "/api/chat",
+  method: "OPTIONS",
+  handler: httpAction(async (_, request) => {
+    const headers = request.headers;
+    if (
+      headers.get("Origin") !== null &&
+      headers.get("Access-Control-Request-Method") !== null &&
+      headers.get("Access-Control-Request-Headers") !== null
+    ) {
+      return new Response(null, {
+        headers: new Headers({
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "POST",
+          "Access-Control-Allow-Headers": "Content-Type, Digest, Authorization",
+          "Access-Control-Max-Age": "86400",
+        }),
+      });
+    } else {
+      return new Response();
+    }
   }),
 });
 
